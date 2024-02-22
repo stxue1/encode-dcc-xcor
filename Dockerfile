1a6fb6766ed6af404da35276312521785081416c
stxue1 <stxue@ucsc.edu>
2024-02-21 19:09:28 -0800

    Add dockerfile and related files

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM quay.io/encode-dcc/xcor:latest
+
+ENV PYTHONPATH .
+# python multiproccessing.cpu_count() is not accurate under slurm
+# https://stackoverflow.com/a/50029433
+# https://github.com/moby/moby/issues/29110#issuecomment-274339807
+USER root
+COPY envwrapper .
+RUN mv envwrapper /bin/envwrapper
+RUN chmod 755 /bin/envwrapper
+#CMD envwrapper /bin/bash
+COPY sitecustomize.py ./sitecustomize.py
+USER encode
+ENTRYPOINT ["envwrapper", "python", "/image_software/pipeline-container/src/xcor.py"]
---
+++ sitecustomize.py
@@ -0,0 +1,37 @@
+import os, sys
+from multiprocessing import cpu_count
+
+def parse_cmdline():
+    env_ncpu = None
+    cmdline_ncpu = None
+
+    env_opt = os.environ.get('PYTHONCPUCOUNT', None)
+    if env_opt:
+        try:
+            ncpu = int(env_opt)
+        except ValueError:
+            print("WARNING: invalid PYTHONCPUCOUNT value:", env_opt)
+        else:
+            env_ncpu = ncpu
+    # I don't think the below is supported in python2?
+    # if 'cpu_count' in sys._xoptions:
+    #     xopt = sys._xoptions['cpu_count']
+    #     try:
+    #         ncpu = int(xopt)
+    #     except ValueError:
+    #         print("WARNING: invalid PYTHONCPUCOUNT value:", xopt)
+    #     else:
+    #         cmdline_ncpu = ncpu
+
+    ncpu = env_ncpu
+    if cmdline_ncpu:
+        ncpu = cmdline_ncpu
+    if ncpu:
+        # Override os.cpu_count()
+        def cpu_count():
+            return ncpu
+        # cpu_count.__doc__ = os.cpu_count.__doc__
+        os.cpu_count = cpu_count
+
+parse_cmdline()
+
